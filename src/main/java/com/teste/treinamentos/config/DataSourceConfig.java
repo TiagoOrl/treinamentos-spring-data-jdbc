80ba37f5de12ce5bac1f70402b46cb0dc57d06c7
TiagoOrl <tiago_orlando@yahoo.com>
2024-01-18 16:09:57 -0300

    feat: first commit

---
+++ src/main/java/com/teste/treinamentos/config/DataSourceConfig.java
@@ -0,0 +1,27 @@
+package com.teste.treinamentos.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+public class DataSourceConfig {
+    @Bean
+    @Primary
+    @ConfigurationProperties("app.datasource.main") // prefixo do application.properties
+    public HikariDataSource hikariDataSource() {
+        return DataSourceBuilder
+                .create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(HikariDataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
