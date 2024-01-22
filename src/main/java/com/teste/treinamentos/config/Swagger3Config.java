6094038a54dba5291d47331da7b5a4e7c4a20322
TiagoOrl <tiago_orlando@yahoo.com>
2024-01-21 22:33:58 -0300

    fix: added authorization option for swagger UI

--- src/main/java/com/teste/treinamentos/config/Swagger3Config.java
+++ src/main/java/com/teste/treinamentos/config/Swagger3Config.java
@@ -1,6 +1,13 @@
 package com.teste.treinamentos.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.customizers.OperationCustomizer;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +22,26 @@ public class Swagger3Config {
                 .displayName("Treinamentos RH")
                 .pathsToMatch("/api/curso/**", "/api/funcionario/**", "/api/turma/**")
                 .build();
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()))
+                .info(
+                        new Info()
+                                .title("Treinamento de Funcionarios Linked RH")
+                                .description("API para cadastro de funcionarios, cursos e turmas de treinamento.")
+                                .license(new License().name("License of API")
+                                .url("API license URL")));
     }
 
 }
