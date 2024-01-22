c0e2e0ab4345634c028c2f2bec87b011e33c2afc
TiagoOrl <tiago_orlando@yahoo.com>
2024-01-21 21:34:52 -0300

    added request interceptor to check authorization token

---
+++ src/main/java/com/teste/treinamentos/middleware/HeaderInterceptor.java
@@ -0,0 +1,33 @@
+package com.teste.treinamentos.middleware;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+@Slf4j
+public class HeaderInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring("Bearer ".length());
+
+            if (token.equals("token_exercicio_rest")) {
+                return true;
+            } else {
+                response.sendError(HttpServletResponse.SC_FORBIDDEN, "Unauthorized");
+                return false;
+            }
+
+        }
+
+        response.sendError(HttpServletResponse.SC_FORBIDDEN, "Unauthorized");
+        return false;
+    }
+}
